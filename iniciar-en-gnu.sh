1c03a5eba24503c2f9ac5a0c300024a81ec094b3
Delmo <delmosaurio@gmail.com>
2014-09-12 21:07:57 -0300

    Create iniciar-en-gnu.sh

---
+++ iniciar-en-gnu.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+
+runpad(){
+	cd app
+
+	node desktop.js >> log.txt &
+	echo "Ejecutando aplicación"
+	waitrun
+	xdg-open http://localhost:8000 
+}
+
+waitrun(){
+	ping localhost -c 5 > /dev/null
+}
+
+installnode(){
+	sudo cp -r bin/gnu-linux/lib/node_modules/ /usr/lib/ # copy the node modules folder to the /lib/ folder
+	sudo cp -r bin/gnu-linux/include/node /usr/include/ # copy the /include/node folder to /usr/local/include folder
+	sudo mkdir /usr/local/man/man1 # create the man folder
+	sudo cp bin/gnu-linux/share/man/man1/node.1 /usr/local/man/man1/ # copy the man file
+	sudo cp bin/gnu-linux/bin/node /usr/bin/ # copy node to the bin folder
+	sudo chmod +x /usr/bin/node
+}
+
+
+if which node >/dev/null; then
+    runpad 
+else
+    echo " Node.js no esta instalado en el equipo"
+    echo " Pof favor espere miestras se instala Node.js"
+    installnode
+    runpad
+fi
