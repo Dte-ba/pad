594cfeb915a2a396f279a6bf56ddc06f293023d2
Delmo <delmosaurio@gmail.com>
2014-11-12 20:42:24 -0300

    fix headers

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.get('/request', function(req, res){
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
+  console.log(req.url + ' Not Found');
   err.status = 404;
   next(err);
 });
@@ -67,7 +68,6 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    console.error(req.url + ' Not Found');
     console.error(err.stack);
     res.render('error', {
       message: err.message,
@@ -80,10 +80,12 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  if (!res.headersSent) {
+    res.render('error', {
+      message: err.message,
+      error: {}
+    });
+  }
 });
 
 var usersOnline = 0;
