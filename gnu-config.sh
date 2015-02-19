325596b951c7757ad1b787583b84eaa28ba698b0
Delmo <delmosaurio@gmail.com>
2015-02-18 23:56:04 -0300

    fix bash

--- gnu-config.sh
+++ gnu-config.sh
@@ -7,19 +7,16 @@ NODE_APP="`which node`"
 
 echo "#!/bin/bash" > $INITD
 echo "# /etc/init.d/pad" >> $INITD
-echo "# Apicación PAD" >> $INITD
-
-echo "APP_PATH=$APP_PATH_VAR" >> $INITD
-
+echo "# Aplicación PAD" >> $INITD
 echo "case \$1 in" >> $INITD
 echo "  start)" >> $INITD
 echo "    echo -n \$\"Starting pad: \"" >> $INITD
-echo "    exec 2>&1 $NODE_APP  \$APP_PATH >> $APP_PATH_DIR/app.log &"  >> $INITD
+echo "    exec 2>&1 $NODE_APP $APP_PATH_VAR >> $APP_PATH_DIR/app.log &"  >> $INITD
 echo "    echo \" [ OK ]\"" >> $INITD
 echo "  ;;" >> $INITD
 echo "  stop)" >> $INITD
 echo "    echo -n \$\"Stoping pad: \"" >> $INITD
-echo "    pkill -f 'node \$APP_PATH'"  >> $INITD
+echo "    pkill -f 'node $APP_PATH_VAR'"  >> $INITD
 echo "    echo \" [ OK ]\"" >> $INITD
 echo "  ;;" >> $INITD
 
