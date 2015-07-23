6ebaefeaf71f6968c7277f6626578dd00ddae100
Delmo <delmosaurio@gmail.com>
2015-07-23 03:52:11 -0300

    clean pad5

--- desktop.js
+++
@@ -1,4 +0,0 @@
-process.env.NODE_ENV = 'production';
-process.env.PORT = process.env.PORT || 8000;
-
-var app = require('./app.js')({ port: process.env.PORT });
--- dev.js
+++
@@ -1,2 +0,0 @@
-
-var app = require('./app.js')({ port: 8000 });
