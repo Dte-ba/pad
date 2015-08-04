b7f905490b58266be0b8d27de752b93837783d4c
Delmo <delmosaurio@gmail.com>
2015-08-04 15:52:45 -0300

    read config from home

--- server/index.js
+++ server/index.js
@@ -6,9 +6,17 @@
 
 var osenv = require('osenv');
 var path = require('path');
+var fs = require('fs');
+
+var localConfig = path.join(osenv.home(), '.pad');
 
 process.env.PAD_MODE = 'server';
-process.env.REPOSITORY_PATH = path.join(osenv.home(), '/repository');
+process.env.REPOSITORY_PATH = path.join(osenv.home());
+
+if (fs.existsSync(localConfig)){
+  var cfg = JSON.parse(fs.readFileSync(localConfig, 'utf-8'));
+  process.env.REPOSITORY_PATH = cfg.path;
+}
 
 require('./app.js')(function(err, app, config){
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
